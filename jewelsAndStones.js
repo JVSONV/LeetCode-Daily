183dd9e3d603cacf319638c78d93454e4bac129c
creatorjv <jvdevelops@gmail.com>
2023-03-16 14:35:43 -0400

    jewels and stones simplfied and without alteration of data

--- jewelsAndStones.js
+++ jewelsAndStones.js
@@ -1,16 +1,17 @@
-var numJewelsInStones = function (jewels, stones) {
-  return stones.split("").reduce((acc, curr) => {
-    jewels.includes(curr) ? (curr = 1) : (curr = 0);
-    return (acc += curr);
-  }, 0);
+var numJewelsInStones = function(jewels, stones) {
+  return stones.split('').filter(letter => jewels.includes(letter)).length;
 };
+  // return stones.split("").reduce((acc, curr)=> {
+  //     jewels.includes(curr) ? curr = 1 : curr = 0;
+  //     return acc+=curr
+  // }, 0);
 
-// stones.split("").forEach(stone => {
-//     if (jewels.includes(stone)) count++;
-// })
+  // stones.split("").forEach(stone => {
+  //     if (jewels.includes(stone)) count++;
+  // })
 
-// stones.split("").forEach((stone,i) => {
-//     jewels.split("").forEach((jewel,j) => {
-//         if(stones.charCodeAt(i) === jewels.charCodeAt(j)) count++ ;
-//     })
-// })
+  // stones.split("").forEach((stone,i) => {
+  //     jewels.split("").forEach((jewel,j) => {
+  //         if(stones.charCodeAt(i) === jewels.charCodeAt(j)) count++ ;
+  //     })
+  // })
